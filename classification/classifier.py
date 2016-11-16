da5bd77863289d233edca3db7a60d2f8266cda1a
Kien Nguyen <ntk148v@gmail.com>
2016-11-16 23:32:56 +0700

    Update _tuning_parameters() method

--- classification/classifier.py
+++ classification/classifier.py
@@ -1,12 +1,13 @@
 import logging
 import numpy as np
-# import pickle
+import pickle
 # from sklearn.model_selection import train_test_split
-# from sklearn.model_selection import GridSearchCV
-# from sklearn.metrics import classification_report
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import classification_report
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 
+from classification import conf
 from classification import utils
 
 
@@ -24,6 +25,11 @@ class Classifier(object):
         """
         self.algorithm = algorithm
         self.text_extract_type = text_extract_type
+        self.estimator = None
+        self.is_saved = False
+        self.pickle_file = conf.SAVED_DIR + self.algorithm + \
+            '_' + self.text_extract_type + '.pickle'
+        self.train()
 
     def load_train_set(self):
         """Load train set from database."""
@@ -37,40 +43,73 @@ class Classifier(object):
             labels.append(p['label'])
         return (contents, labels)
 
-    def _set_estimator(self, params):
-        if self.algorithm is 'SVM':
-            self.estimator = SVC(kernel=params['kernel'], C=params['C'],
-                                 gamma=params['gamma'])
-        else:
-            self.estimator = KNeighborsClassifier(
-                n_neighbors=params['n_neighbors'],
-                metric=params['metric'])
+    # def _set_estimator(self, params):
+    #     """Set estimator instance.
+
+    #     :param dict params: parameters.
+    #     """
+    #     if self.algorithm == 'SVM':
+    #         if params['kernel'] == 'rbf':
+    #             self.estimator = SVC(kernel=params['kernel'], C=params['C'],
+    #                                  gamma=params['gamma'])
+    #         if params['kernel'] == 'linear':
+    #             self.estimator = SVC(kernel=params['kernel'], C=params['C'])
+    #     else:
+    #         self.estimator = KNeighborsClassifier(
+    #             n_neighbors=params['n_neighbors'],
+    #             metric=params['metric'])
 
     def word_to_vector(self):
         """Convert from text to vector."""
         contents, labels = self.load_train_set()
-        if self.text_extract_type is 'Bag Of Words':
+        if self.text_extract_type == 'Bag Of Words':
             self.X = utils.bag_of_words(contents)
         else:
             self.X = utils.td_idf(contents)
 
         self.y = np.array(labels)
 
-    def _tunning_parameters(self):
+    def _tuning_parameters(self):
         """Tuning the hyper-parameters of an estimator."""
-        self._set_estimator()
+
+        if self.algorithm == 'SVM':
+            tmp_estimator = SVC(C=1)
+            pre_tune_params = conf.SVM_PARAMS
+        else:
+            tmp_estimator = KNeighborsClassifier()
+            pre_tune_params = conf.KNN_PARAMS
+
+        grid_search = GridSearchCV(tmp_estimator, pre_tune_params, cv=5)
+        grid_search.fit(self.X, self.y)
+        LOG.info('Tuning the hyper-parameters of an {} estimator' .
+                 format(self.algorithm))
+        self.estimator = grid_search.best_estimator_
 
     def train(self):
-        pass
+        self.word_to_vector()
+        if not self.estimator:
+            self._tuning_parameters()
+        else:
+            self.estimator.fit(self.X)
 
     def predict(self, X):
-        pass
+        self.estimator.predict(X)
 
     def evaluate(self, X, y, estimator, test_size=0.4, confusion=False):
         pass
 
-    def save_estimator(self, clf):
-        pass
+    def save(self):
+        """Save Classifier object to file with pickle."""
+        LOG.info('Saving Classifier object to {}' . format(self.pickle_file))
+        pickle.dump(self, open(self.pickle_file, 'wb'))
+        self.is_saved = True
 
-    def load_estimator(self):
-        pass
+    def load(self):
+        """Load Classifier object from file with pickle."""
+        if self.is_saved:
+            LOG.info('Loading saved Classifier object from {}' .
+                     format(self.pickle_file))
+            return pickle.loads(open(self.pickle_file, 'rb'))
+        else:
+            LOG.info('Unsaved object, try Classifier.save()')
+            return None
