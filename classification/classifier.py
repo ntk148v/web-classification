f513378a70835788de7fe40ef7e5d58e5601b2fe
Kien Nguyen <ntk148v@gmail.com>
2016-11-19 00:27:10 +0700

    Update with Multiclass + SVC

--- classification/classifier.py
+++ classification/classifier.py
@@ -1,9 +1,10 @@
 import logging
 import numpy as np
 import pickle
+# from sklearn.metrics import classification_report
 # from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV
-# from sklearn.metrics import classification_report
+from sklearn.multiclass import OneVsOneClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 
@@ -18,22 +19,25 @@ class Classifier(object):
 
     """Classifier class"""
 
-    def __init__(self, algorithm, text_extract_type):
+    def __init__(self, algorithm, text_extract_type, language):
         """
         :param str algorithm: choosen algorithm will be used to classify.
         :param str text_extract_type: text extraction (Bag of Words & TF_IDF).
+        :param str language: language (vi or en).
         """
         self.algorithm = algorithm
         self.text_extract_type = text_extract_type
+        self.language = language
         self.estimator = None
         self.is_saved = False
         self.pickle_file = conf.SAVED_DIR + self.algorithm + \
-            '_' + self.text_extract_type + '.pickle'
+            '_' + self.text_extract_type + '-' + self.language + '.pickle'
         self.train()
 
     def load_train_set(self):
         """Load train set from database."""
-        query = "SELECT `id`, `content`, `label`, `link` FROM `news_dantri`"
+        query = "SELECT `id`, `content`, `label`, `link` FROM " +\
+                conf.TRAINING_TABLE
         train_set = utils.get_data_from_db(query, 10000)
 
         contents = []
@@ -42,22 +46,6 @@ class Classifier(object):
             contents.append(p['content'])
             labels.append(p['label'])
         return (contents, labels)
-
-    # def _set_estimator(self, params):
-    #     """Set estimator instance.
-
-    #     :param dict params: parameters.
-    #     """
-    #     if self.algorithm == 'SVM':
-    #         if params['kernel'] == 'rbf':
-    #             self.estimator = SVC(kernel=params['kernel'], C=params['C'],
-    #                                  gamma=params['gamma'])
-    #         if params['kernel'] == 'linear':
-    #             self.estimator = SVC(kernel=params['kernel'], C=params['C'])
-    #     else:
-    #         self.estimator = KNeighborsClassifier(
-    #             n_neighbors=params['n_neighbors'],
-    #             metric=params['metric'])
 
     def word_to_vector(self):
         """Convert from text to vector."""
@@ -83,7 +71,8 @@ class Classifier(object):
         grid_search.fit(self.X, self.y)
         LOG.info('Tuning the hyper-parameters of an {} estimator' .
                  format(self.algorithm))
-        self.estimator = grid_search.best_estimator_
+        self.estimator = OneVsOneClassifier(grid_search.best_estimator_) \
+            if self.algorithm == 'SVM' else grid_search.best_estimator_
 
     def train(self):
         self.word_to_vector()
