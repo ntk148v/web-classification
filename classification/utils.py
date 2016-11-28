e1914c5a480bb7edc9bc571f98ee63d2c02b9e0f
Kien Nguyen <ntk148v@gmail.com>
2016-11-28 17:06:50 +0700

    Update
    - Add evalute() method
    - Update connection to use both mysql and sqlite engine.

--- classification/utils.py
+++ classification/utils.py
@@ -1,8 +1,11 @@
+import logging
+import nltk
 import os
 import pickle
 import pymysql
-import logging
-import nltk
+import sqlite3
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 
 from classification import conf
@@ -60,22 +63,32 @@ def td_idf(data, is_eng=True):
     return tf_transformers
 
 
-def get_data_from_db(query, rows):
+def get_data_from_db(query, rows, engine=conf.DB_ENGINE):
     """Connect to database and get data from it.
 
     :param str query: sql query.
     :param int rows: number of rows.
+    :param str engine: database engine.
+                       default is MySQL.
     :rtype: list
     """
+    # Initialize `connection` var to None. In case we could not
+    # create a connection to the database(for ex the disk is full)
+    # we would not have a connection var defined.
+    connection = None
     try:
-        connection = pymysql.connect(host=conf.MYSQL_HOST,
-                                     port=conf.MYSQL_PORT,
-                                     user=conf.MYSQL_USER,
-                                     password=conf.MYSQL_PASSWORD,
-                                     db=conf.MYSQL_DB,
-                                     charset='utf8mb4',
-                                     cursorclass=pymysql.cursors.DictCursor)
-        LOG.info('Connect to db {}' . format(conf.MYSQL_DB))
+        if engine.lower() == 'mysql':
+            connection = pymysql.connect(host=conf.MYSQL_HOST,
+                                         port=conf.MYSQL_PORT,
+                                         user=conf.MYSQL_USER,
+                                         password=conf.MYSQL_PASSWORD,
+                                         db=conf.DB,
+                                         charset='utf8mb4',
+                                         cursorclass=pymysql.cursors.
+                                         DictCursor)
+        elif engine.lower() == 'sqlite':
+            connection = sqlite3.connect(conf.DB)
+        LOG.info('Connect to {} db - {}' . format(engine, conf.DB))
 
         with connection.cursor() as cursor:
             cursor.execute(query)
@@ -85,8 +98,9 @@ def get_data_from_db(query, rows):
     except Exception as e:
         LOG.exception('Failed when connecting to database: {}. ' . format(e))
     finally:
-        LOG.info('Close database connection.')
-        connection.close()
+        if connection:
+            LOG.info('Close database connection.')
+            connection.close()
 
 
 def save(obj, path):
@@ -102,3 +116,23 @@ def load(path):
         raise IOError()
     else:
         return pickle.load(open(path, 'rb'))
+
+
+def evaluate(self, X, y, estimator, test_size=0.4, confusion=False):
+    """Evaluate algorithm.
+    :param numpy matrix X: dataset.
+    :param numpy matrix y: label matrix.
+    :param float test_size: testset's size/alldataset's size.
+    :param boolean confusion: use confusion matrix or not.
+    """
+    LOG.info('Evaluate with test_size is %2.0f%%' % (test_size * 100))
+    # Split into a training and testing set
+    X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                        test_size=test_size)
+    estimator.fit(X_train, y_train)
+    y_predicted = estimator.predict(X_test)
+
+    if not confusion:
+        return classification_report(y_test, y_predicted)
+    else:
+        return confusion_matrix(y_test, y_predicted)
